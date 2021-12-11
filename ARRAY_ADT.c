988b3349e6d0c1fc870ec0f453b28f2a5c29a32c
Yatendra Kumawat <82805918+virat101998@users.noreply.github.com>
2021-12-12 02:39:42 +0530

    Add files via upload

---
+++ ARRAY_ADT.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct myarray
+{
+    int total_size;
+    int used_size;
+    int *ptr;
+
+};
+
+void createarray(struct myarray* a, int tsize, int usize){
+    //(*a).total_size = tsize;
+    //(*a).used_size = usize;
+    //(*a).ptr = (int *) malloc(tsize*sizeof(int));
+
+    a->total_size = tsize;
+    a->used_size = usize;
+    a->ptr = (int *)malloc(tsize*sizeof(int));
+
+}
+
+
+void show(struct myarray *a){
+    for (int i=0;i < a->used_size;i++)
+    {
+        printf("%d\n", (a->ptr)[i]);
+    }
+}
+
+void setval(struct myarray *a){
+    int n;
+    for (int i=0;i < a->used_size;i++)
+    {
+        printf("Enter element %d", i);
+        scanf("%d", &n);
+        (a->ptr)[i] = n;
+    }
+}
+
+int main(){
+    struct myarray marks;
+    createarray(&marks, 10, 2);
+    setval(&marks);
+    show(&marks);
+
+    return 0;
+}
